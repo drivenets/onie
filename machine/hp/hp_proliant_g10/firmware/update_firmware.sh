5cf36492d444dcf7534d5455f2397a160e006d25
Marco Supino <marco@drivenets.com>
2022-11-17 13:11:43 +0000

    fix hp firmware wrapper

--- machine/hp/hp_proliant_g10/firmware/update_firmware.sh
+++ machine/hp/hp_proliant_g10/firmware/update_firmware.sh
@@ -2,10 +2,14 @@
 
 mnt_dir=/mnt/tmp
 firmware_script=${mnt_dir}/ncc_firmware_update.sh
+
 PART_LABEL=DNOS-TMP
+DISK_PATH=/dev/sda
+
+PART_NUM=$(/usr/bin/sgdisk -p ${DISK_PATH} | grep ${PART_LABEL} | awk '{print $1}')
 
 mkdir -p ${mnt_dir}
-mount LABEL=${PART_LABEL} ${mnt_dir}
+mount ${DISK_PATH}${PART_NUM} ${mnt_dir}
 
 cd ${mnt_dir}
 
